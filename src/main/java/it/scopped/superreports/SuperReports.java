98a03608292f29f8e7d46dc6a9e9bdad42ae6a82
Scopped <coderdev23@gmail.com>
2024-11-03 03:25:25 +0100

    - Project Created

---
+++ src/main/java/it/scopped/superreports/SuperReports.java
@@ -0,0 +1,16 @@
+package it.scopped.superreports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperReports extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
